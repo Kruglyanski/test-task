8f80f50a6ae5f57f5f082ee47e88c97a685fee00
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-02-20 10:57:09 +0300

    feat: implement server

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('port')
+
+app.get('/', (request, response) => {
+    response.send('Hell!')
+})
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'),{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}!!!`))
+    } catch (e) {
+        console.log('Server ERROR', e.message)
+        process.exit(1)
+    }
+}
+
+start()
