3b85e66d191d38bbc6c1649b1d478d40362ce9c4
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-02-24 21:26:43 +0300

    fix: server 2

--- src/server.ts
+++ src/server.ts
@@ -43,11 +43,11 @@ app.post("/api/auth/upload", async function (req, res, next) {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client/build')))
+    app.use('/', express.static(__dirname + '/client/build'))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
-    } )
+    // app.get('*', (req,res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    // } )
 }
 app.use('/uploads', express.static(__dirname + '/uploads'))
 async function start() {
