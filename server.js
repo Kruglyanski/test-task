d896a5b4d1901f9f7fefbb4d781b44d8af9c60a1
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-02-25 11:38:19 +0300

    fix: +

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html'))
     } )
 }
-app.use('/uploads', express.static(path.join(__dirname , 'uploads')))
+app.use('/uploads', express.static( '/uploads'))
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'),{
